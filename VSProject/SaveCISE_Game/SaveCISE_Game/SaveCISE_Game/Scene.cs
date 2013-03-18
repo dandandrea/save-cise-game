3fed3b26068f5e434402c720455e73c5516e1e3e
VathauerJE@gmail.com <VathauerJE@gmail.com@2baa5a33-f81b-191d-4f15-b713e5c9db51>
2013-03-18 06:05:39 +0000

    Created skeleton for the basic graphics system: added Sprite, Actor, Scene, Stage, ContentStore.  I also added an example background and SpriteFont that show the convention I think we should stick to for naming content files (prefix with bg_, spr_, font_, followed by the name), and the code shows how to load them into the game.

---
+++ VSProject/SaveCISE_Game/SaveCISE_Game/SaveCISE_Game/Scene.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SaveCISE_Game
+{
+    class Scene
+    {
+        Sprite background;
+        List<Actor> actors;
+
+        public Scene()
+        {
+            actors = new List<Actor>();
+        }
+
+        public void setBackground(Sprite background)
+        {
+            this.background = background;
+        }
+
+        public void add(Actor actor)
+        {
+            this.actors.Add(actor);
+        }
+
+        public void draw( SpriteBatch sb )
+        {
+            if (background != null)
+            {
+                background.draw(sb, 0, 0, 0);
+            }
+            foreach (Actor a in actors)
+            {
+                a.draw(sb);
+            }
+        }
+    }
+}
