4ad94f73c75b94b145913ffcc0b8e212b551ea29
VathauerJE@gmail.com <VathauerJE@gmail.com@2baa5a33-f81b-191d-4f15-b713e5c9db51>
2013-04-05 01:26:30 +0000

    Drawing the grid.

---
+++ VSProject/SaveCISE_Game/SaveCISE_Game/SaveCISE_Game/GridDrawer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace SaveCISE_Game
+{
+    class GridDrawer : Actor
+    {
+        public GridDrawer() : base(new Sprite(ContentStore.getTexture("spr_whitePixel"),GameController.CELL_WIDTH-2,GameController.CELL_HEIGHT-2,1,1))
+        {
+
+        }
+        public override void draw(Microsoft.Xna.Framework.Graphics.SpriteBatch sb)
+        {
+            for (int i = 0; i < GameController.GRID_WIDTH; i++)
+            {
+                for (int j = 0; j < GameController.GRID_HEIGHT; j++)
+                {
+                    Color alpha = new Color(100,100,100,100);
+                    this.sprite.draw(sb, i * GameController.CELL_WIDTH+GameController.GRID_OFFSET_X, j * GameController.CELL_HEIGHT+GameController.GRID_OFFSET_Y, 0,1,1,alpha);
+                }
+            }
+        }
+    }
+}
