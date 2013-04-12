c8455535abacd464f92a8b9abe830d0485e4eff6
VathauerJE@gmail.com <VathauerJE@gmail.com@2baa5a33-f81b-191d-4f15-b713e5c9db51>
2013-04-12 00:18:45 +0000

    Added BudgetDrawer.cs.

---
+++ VSProject/SaveCISE_Game/SaveCISE_Game/SaveCISE_Game/BudgetDrawer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SaveCISE_Game
+{
+    class BudgetDrawer : Actor
+    {
+        int maxBarHeight = 300;
+        public BudgetDrawer() : base(new Sprite(ContentStore.getTexture("spr_whitePixel")))
+        {
+            this.width = 30;
+            this.x = 600;
+            this.y = 30;
+        }
+
+        public override void draw(SpriteBatch sb)
+        {
+            base.draw(sb, Color.Green);
+        }
+
+        public override void update()
+        {
+            this.height = (int)(((float)GameController.getBudget() / GameController.MAX_BUDGET)*this.maxBarHeight);
+        }
+    }
+}
