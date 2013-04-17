bd7a324fdfbb8777ea8cc7e141c3ea4a6eccb25d
juntaozhu15@gmail.com <juntaozhu15@gmail.com@2baa5a33-f81b-191d-4f15-b713e5c9db51>
2013-04-17 02:15:02 +0000

    MOAR BALANCE!!

--- trunk/VSProject/SaveCISE_Game/SaveCISE_Game/SaveCISE_Game/GameController.cs
+++ trunk/VSProject/SaveCISE_Game/SaveCISE_Game/SaveCISE_Game/GameController.cs
@@ -491,21 +491,21 @@ namespace SaveCISE_Game
             waves = new List<List<Enemy>>();
 
             // waves 1-5
-            waves.Add(mobFactory.generateMob1(15, 1f, 20, 2500, 10));
-            waves.Add(mobFactory.generateMob1(15, 1.5f, 30, 2500, 15));
-            waves.Add(mobFactory.generateMob2(60, 3.8f, 10, 1000, 5));
+            waves.Add(mobFactory.generateMob1(15, 1.5f, 20, 2500, 10));
+            waves.Add(mobFactory.generateMob1(20, 1.5f, 30, 2500, 10));
+            waves.Add(mobFactory.generateMob2(60, 3.8f, 11, 1000, 5));
             waves.Add(mobFactory.generateMob3(5, 1f, 400, 2500, 80));
             waves.Add(mobFactory.generateMob1(25, 1.5f, 120, 2500, 20));
             // boss 1
-            waves.Add(mobFactory.generateBoss1(1, 2f, 700, 250000, 1000));
+            waves.Add(mobFactory.generateBoss1(1, 2f, 700, 250000, 600));
             // waves 6-10
-            waves.Add(mobFactory.generateMob1(30, 1.5f, 180, 2500, 20));
+            waves.Add(mobFactory.generateMob1(30, 1.5f, 200, 2500, 20));
             waves.Add(mobFactory.generateMob3(7, 1f, 700, 2500, 100));
-            waves.Add(mobFactory.generateMob2(120, 4f, 20, 2500, 5));
-            waves.Add(mobFactory.generateMob1(50, 1.5f, 200, 2500, 20));
-            waves.Add(mobFactory.generateMob3(15, 1f, 900, 2500, 50));
+            waves.Add(mobFactory.generateMob2(150, 3.8f, 20, 2500, 5));
+            waves.Add(mobFactory.generateMob1(50, 1.5f, 220, 2500, 20));
+            waves.Add(mobFactory.generateMob3(15, 1f, 1000, 2500, 50));
             // boss 2
-            waves.Add(mobFactory.generateBoss2(1, 2f, 2750, 250000, 0));
+            waves.Add(mobFactory.generateBoss2(1, 2f, 2600, 250000, 0));
 
 
             #if DEBUG
