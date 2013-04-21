e713e2770f5064bf0ee7744f1bc3e84ea6156d4f
dan.dandrea <dan.dandrea@2baa5a33-f81b-191d-4f15-b713e5c9db51>
2013-04-21 20:39:32 +0000

    Added restart button (still needs to have a new image created for it).

---
+++ trunk/VSProject/SaveCISE_Game/SaveCISE_Game/SaveCISE_Game/ReplayGameAction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SaveCISE_Game
+{
+    class ReplayGameAction : GameAction
+    {
+        Stage stage;
+        int index;
+        Scene scene;
+        public ReplayGameAction(Stage stage, int index, Scene scene)
+        {
+            this.stage = stage;
+            this.index = index;
+            this.scene = scene;
+        }
+        public override void doAction()
+        {
+            GameController.reset();
+            stage.removeScene(scene);
+            scene = GameController.getGameScene();
+            stage.insertScene(index, scene);
+            stage.gotoScene(0);
+        }
+    }
+}
